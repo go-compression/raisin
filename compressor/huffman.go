301da4da8d2e2f27019dbaafb85ad5b7c7662b1e
Arnav Chawla <arnavchawla23@gmail.com>
2020-07-09 13:21:37 -0700

    Update huffman.go

--- compressor/huffman.go
+++ compressor/huffman.go
@@ -1,19 +1,3 @@
-<?php
-$name = get_post_meta($post->ID, "certified_user_name", true);
-$course = get_post_meta($post->ID, "course_name", true);
-?>
-<div style="width:800px; height:600px; padding:20px; text-align:center; border: 10px solid #787878">
-<div style="width:750px; height:550px; padding:20px; text-align:center; border: 5px solid #787878">
-       <span style="font-size:50px; font-weight:bold">Certificate of Completion</span>
-       <br><br>
-       <span style="font-size:25px"><i>This is to certify that</i></span>
-       <br><br>
-       <span style="font-size:30px"><b><?php echo $name; ?></b></span><br/><br/>
-       <span style="font-size:25px"><i>has completed the course</i></span> <br/><br/>
-       <span style="font-size:30px"><?php echo $course; ?></span> <br/><br/>
-</div>
-</div>
-
 package main
 
 import (
