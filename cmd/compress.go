eaa1bbbe2e60d1b8288951b4cbfa79aa67be81d8
Phillip Cutter <mrfleap@gmail.com>
2020-07-10 17:26:49 -0700

    Forgot to add compress.go

--- cmd/compress.go
+++ cmd/compress.go
@@ -8,30 +8,31 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var compressCmd = &cobra.Command{
-	Use:   "compress",
-	Short: "Compress a file using custom-compressor",
-	Long:  `Prints version number and build info`,
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
-			return errors.New("requires a file argument to compress")
-		}
-		file := args[0]
-		if _, err := os.Stat(file); os.IsNotExist(err) {
-			return fmt.Errorf("Could not open file (likely does not exist): %s", args[0])
-		} else {
-			return nil
-		}
-
-	},
-	Run: compress,
-}
-
 func init() {
-	rootCmd.AddCommand(compressCmd)
-}
+	var maxSearchBuffer int
 
-func compress(cmd *cobra.Command, args []string) {
-	file := args[0] // Args[0] = file as a string
-	engine.CompressFile(file)
+	var compressCmd = &cobra.Command{
+		Use:   "compress",
+		Short: "Compress a file using custom-compressor",
+		Long:  `Prints version number and build info`,
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("requires a file argument to compress")
+			}
+			file := args[0]
+			if _, err := os.Stat(file); os.IsNotExist(err) {
+				return fmt.Errorf("Could not open file (likely does not exist): %s", args[0])
+			} else {
+				return nil
+			}
+
+		},
+		Run: func (cmd *cobra.Command, args []string) {
+			file := args[0] // Args[0] = file as a string
+			engine.CompressFile(file, maxSearchBuffer)
+		},
+	}
+
+	compressCmd.PersistentFlags().IntVarP(&maxSearchBuffer, "max search buffer length", "m", 1024, "maximum length of search buffer")
+	rootCmd.AddCommand(compressCmd)
 }
